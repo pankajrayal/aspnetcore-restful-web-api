edbb9cdc4bd2f0cb79e849a863496e4be233d470
Pankaj Rayal <pankajrayal@gmail.com>
2021-05-29 11:05:25 +0530

    04-Resource Manipulation-Start

---
+++ 04-Resource Manipulation/MoviesAPI/MoviesAPI/Controllers/GenresController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MoviesAPI.Entities;
+using MoviesAPI.Filters;
+using MoviesAPI.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/genres")]
+    [ApiController]
+    public class GenresController: ControllerBase
+    {
+        private readonly IRepository repository;
+        private readonly ILogger<GenresController> logger;
+
+        public GenresController(IRepository repository, ILogger<GenresController> logger)
+        {
+            this.repository = repository;
+            this.logger = logger;
+        }
+
+        [HttpGet] // api/genres
+        public async Task<ActionResult<List<Genre>>> Get()
+        {
+            return await repository.GetAllGenres();
+        }
+
+        [HttpGet("{Id:int}", Name = "getGenre")] // api/genres/example
+        [ServiceFilter(typeof(MyActionFilter))]
+        public ActionResult<Genre> Get(int Id, string param2)
+        {
+            var genre = repository.GetGenreById(Id);
+
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return genre;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Genre genre)
+        {
+            repository.AddGenre(genre);
+
+            return new CreatedAtRouteResult("getGenre", new { Id = genre.Id }, genre);
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Genre genre)
+        {
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            return NoContent();
+
+        }
+    }
+}
---
+++ 04-Resource Manipulation/MoviesAPI/MoviesAPI/Validators/FirstLetterUppercaseAttribute.cs
@@ -0,0 +1,30 @@
+﻿using MoviesAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Validators
+{
+    public class FirstLetterUppercaseAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext) 
+        {
+            //var genre = (Genre) validationContext.ObjectInstance;
+            //genre.
+
+            if(value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+
+            var firstletter = value.ToString()[0].ToString();
+            if (firstletter != firstletter.ToUpper()) {
+                return new ValidationResult("First letter should be uppercase");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
